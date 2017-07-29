9b577eea5fb98d1d6a45f6885686854599156cae
- <@>
2017-07-29 22:15:57 +0300

    rm some outdated polyfills

--- test/collectdoctests.js
+++ test/collectdoctests.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 const fs=require('fs')
-const trim=x=>x.replace(/(^ +| +$)/g,'')
-var t=[] //tests
+const t=[] //tests
 const visit=d=>{
   fs.readdirSync(d).forEach(f=>{
     const df=d+'/'+f;if(fs.lstatSync(df).isDirectory()){visit(df);return}
-    if(!/^(\w|\.)+$/.test(f))return
     var a=fs.readFileSync(df,'utf8').split('\n'),i=0
     while(i<a.length){
       var s=a[i++],m
       while(i<a.length&&(m=a[i].match(/^ *(?:#|⍝|\/\/) *\.\.\.(.*)$/))){s+='\n'+m[1];i++}
-      if(m=s.match(/^ *(?:#|⍝|\/\/) ([^]*)(←→|!!!)([^]+)$/))t.push([trim(m[1]),trim(m[2]),trim(m[3])])
+      if(m=s.match(/^ *(?:#|⍝|\/\/) ([^]*)(←→|!!!)([^]+)$/))t.push([m[1].trim(),m[2].trim(),m[3].trim()])
     }
   })
 }
