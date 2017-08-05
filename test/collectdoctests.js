b9bf363d2211bed562cb90a67d1d6a6254794f84
- <@>
2017-08-05 17:39:10 +0300

    rewrote collectdoctests.js; use ⍙ as doctest separator

--- test/collectdoctests.js
+++ test/collectdoctests.js
@@ -1,14 +1,7 @@
 #!/usr/bin/env node
-const fs=require('fs')
-const t=[]
-const visit=d=>{
-  fs.readdirSync(d).forEach(f=>{
-    const df=d+'/'+f;if(fs.lstatSync(df).isDirectory()){visit(df);return}
-    const a=fs.readFileSync(df,'utf8').split('\n')
-    for(var i=0;i<a.length;i++){
-      const m=a[i].match(/(?:⍝|\/\/) (.*)(←→|!!!)(.+)$/);if(m)t.push([m[1].trim(),m[2].trim(),m[3].trim()])
-    }
-  })
-}
-visit(__dirname+'/../src')
+var fs=require('fs')
+,f=x=>fs.lstatSync(x).isDirectory()?fs.readdirSync(x).map(y=>f(x+'/'+y)).join(''):fs.readFileSync(x)
+,s=f(__dirname+'/../src'),t=[],m,p=0
+while(m=s.slice(p).match(/(?:⍝|\/\/ |⍙)([^\n⍙]*)(←→|!!!)([^\n⍙]+)/m))
+ {t.push([m[1].trim(),m[2].trim(),m[3].trim()]);p+=m[0].length}
 process.stdout.write('[\n'+t.map(JSON.stringify).join(',\n')+'\n]\n')
