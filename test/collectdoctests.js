2a71aa3702397d657fb0a57ebde1f266ec946a20
- <@>
2017-08-06 19:32:24 +0300

    avoid collecting duplicate tests

--- test/collectdoctests.js
+++ test/collectdoctests.js
@@ -3,5 +3,5 @@ var fs=require('fs')
 ,f=x=>fs.lstatSync(x).isDirectory()?fs.readdirSync(x).map(y=>f(x+'/'+y)).join(''):fs.readFileSync(x)
 ,s=f(__dirname+'/../src'),t=[],m,p=0
 while(m=s.slice(p).match(/(?:⍝|\/\/ |⍙)([^\n⍙]*)(←→|!!!)([^\n⍙]+)/m))
- {t.push([m[1].trim(),m[2].trim(),m[3].trim()]);p+=m[0].length}
+ {t.push([m[1].trim(),m[2].trim(),m[3].trim()]);p+=m.index+m[0].length}
 process.stdout.write('[\n'+t.map(JSON.stringify).join(',\n')+'\n]\n')
