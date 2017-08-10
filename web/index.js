08b6ac48238fb5ae0bf8f037dc7a265cfb76f1ab
- <@>
2017-08-10 22:16:03 +0300

    new demo page; lang bar instead of kbd; re-added apl385

--- web/index.js
+++ web/index.js
@@ -1,187 +1,78 @@
-document.onready=_=>{
-  var a=document.body.querySelectorAll('[id]'),I={};for(var i=0;i<a.length;i++)I[a[i].id]=a[i]
-
-  // bookmarkable source code
-  var hashParams={}
-  if(location.hash){
-    var a=location.hash.substring(1).split(',')
-    for(var i=0;i<a.length;i++){var b=a[i].split('=');hashParams[b[0]]=unescape(b[1])}
+;(_=>{
+const a=document.body.querySelectorAll('[id]'),I={};for(let i=0;i<a.length;i++)I[a[i].id]=a[i]
+const hp={} // hash params
+if(location.hash){const a=location.hash.substring(1).split(',');for(let i=0;i<a.length;i++){const[k,v]=a[i].split('=')
+                  hp[k]=unescape(v)}}
+I.code.value=hp.code||'';I.code.focus()
+I.perm.onmouseover=I.perm.onfocus=_=>{I.perm.href='#code='+escape(I.code.value);return!1}
+I.go.onclick=_=>{
+  try{
+    const s=I.code.value;I.result.classList.remove('err')
+    if(s===')t'){I.result.textContent='Running tests...';setTimeout(runDocTests,1)}
+    else{I.result.textContent=apl.fmt(apl(s)).join('\n')+'\n'}
+  }catch(e){
+    console&&console.error&&console.error(e.stack);I.result.classList.add('err');I.result.textContent=e
   }
-  I.code.value=hashParams.code||'';I.code.focus()
-  $(I.permalink).tipsy({gravity:'e',opacity:1,delayIn:1000})
-  I.permalink.onmouseover=I.permalink.onfocus=_=>{I.permalink.href='#code='+escape(I.code.value);return!1}
-
-  const execute=_=>{
-    try{
-      var s=I.code.value;I.result.classList.remove('error')
-      if(s===')t'){I.result.textContent='Running tests...';setTimeout(runDocTests,1)}
-      else{I.result.textContent=apl.fmt(apl(s)).join('\n')+'\n'}
-    }catch(e){
-      console&&console.error&&console.error(e.stack)
-      I.result.classList.add('error');I.result.textContent=e
-    }
-  }
-
-  $(I.go).tipsy({gravity:'e',opacity:1,delayIn:1000})
-  I.go.onclick=_=>{execute();return!1}
-  hashParams.run&&I.go.click()
-
-  var hSymbolDefs={
-    '+':'Conjugate, Add',
-    '-':'Negate, Subtract',
-    '×':'Sign of, Multiply',
-    '÷':'Reciprocal, Divide',
-    '⌈':'Ceiling, Greater of',
-    '⌊':'Floor, Lesser of',
-    '|':'Absolute value, Residue',
-    '⍳':'Index generator, Index of',
-    '?':'Roll, Deal',
-    '*':'Exponential, To the power of',
-    '⍟':'Natural logarithm, Logarithm to the base',
-    '○':'Pi times, Circular and hyperbolic functions',
-    '!':'Factorial, Binomial',
-    '⌹':'Matrix inverse, Matrix divide',
-    '⍠':'Variant operator',
-    '<':'Less than',
-    '≤':'Less than or equal',
-    '=':'Equal',
-    '≥':'Greater than or equal',
-    '>':'Greater than',
-    '≠':'Not equal',
-    '≡':'Depth, Match',
-    '≢':'Tally, Not match',
-    '∊':'Enlist, Membership',
-    '⍷':'Find',
-    '∪':'Unique, Union',
-    '∩':'Intersection',
-    '~':'Not, Without',
-    '∨':'Or (Greatest Common Divisor)',
-    '∧':'And (Least Common Multiple)',
-    '⍱':'Nor',
-    '⍲':'Nand',
-    '⍴':'Shape of, Reshape',
-    ',':'Ravel, Catenate',
-    '⍪':'First axis catenate',
-    '⌽':'Reverse, Rotate',
-    '⊖':'First axis rotate',
-    '⍉':'Transpose',
-    '↑':'First, Take',
-    '↓':'Drop',
-    '⊂':'Enclose, Partition',
-    '⊃':'Mix, Pick',
-    '⌷':'Index',
-    '⍋':'Grade up',
-    '⍒':'Grade down',
-    '⊤':'Encode',
-    '⊥':'Decode',
-    '⍕':'Format, Format by specification',
-    '⍎':'Execute',
-    '⊣':'Stop, Left',
-    '⊢':'Pass, Right',
-    '⎕':'Evaluated input, Output with a newline',
-    '⍞':'Character input, Bare output',
-    '¨':'Each',
-    '∘':'Compose',
-    '/':'Reduce',
-    '⌿':'1st axis reduce',
-    '\\':'Scan',
-    '⍀':'1st axis scan',
-    '⍣':'Power operator',
-    '⍨':'Commute',
-    '¯':'Negative number sign',
-    '∞':'Infinity',
-    '⍝':'Comment',
-    '←':'Assignment',
-    '⍬':'Zilde',
-    '⋄':'Statement separator',
-    '⍺':'Left formal parameter',
-    '⍵':'Right formal parameter',
-    APL:'Press backquote (`) followed by another key to insert an APL symbol, e.g. `r inserts rho (⍴)'
-  }
-
-  // Keyboard
-  var layout={
-    'default':[
-      '` 1 2 3 4 5 6 7 8 9 0 - =',
-      'q w e r t y u i o p [ ] \\',
-      'a s d f g h j k l ; \' {enter}',
-      '{shift} z x c v b n m , . / {bksp}',
-      '{alt} {space} {exec!!}'
-    ],
-    shift:[
-      '~ ! @ # $ % ^ & * ( ) _ +',
-      'Q W E R T Y U I O P { } |',
-      'A S D F G H J K L : " {enter}',
-      '{shift} Z X C V B N M < > ? {bksp}',
-      '{alt} {space} {exec!!}'
-    ],
-    alt:[
-      '{empty} ¨ ¯ < ≤ = ≥ > ≠ ∨ ∧ ÷ ×',
-      '{empty} ⍵ ∊ ⍴ ~ ↑ ↓ ⍳ ○ ⍟ ← → ⍀',
-      '⍺ ⌈ ⌊ ⍫ ∇ ∆ ∘ {empty} ⎕ ⋄ {empty} {enter}',
-      '{shift} ⊂ ⊃ ∩ ∪ ⊥ ⊤ | ⍪ {empty} ⌿ {bksp}',
-      '{alt} {space} {exec!!}'
-    ],
-    'alt-shift':[
-      '⍨ ∞ ⍁ ⍂ ⍠ ≈ ⌸ ⍯ ⍣ ⍱ ⍲ ≢ ≡',
-      '⌹ ⍹ ⍷ ⍤ {empty} ⌶ ⊖ ⍸ ⍬ ⌽ ⊣ ⊢ ⍉',
-      '⍶ {empty} {empty} {empty} ⍒ ⍋ ⍝ {empty} ⍞ {empty} {empty} {enter}',
-      '{shift} ⊆ ⊇ ⋔ ⍦ ⍎ ⍕ ⌷ « » ↗ {bksp}',
-      '{alt} {space} {exec!!}'
-    ]
-  }
-
-  // Key mappings
-  var combos={'`':{}}
-  var asciiKeys=layout['default'].concat(layout.shift).join(' ').split(' ')
-  var aplKeys=layout.alt.concat(layout['alt-shift']).join(' ').split(' ')
-  for(var i=0;i<asciiKeys.length;i++){
-    var k=asciiKeys[i],v=aplKeys[i]
-    if(!/^\{\w+\}$/.test(k)&&!/^\{\w+\}$/.test(v))combos['`'][k]=v
-  }
-
-  $.keyboard.keyaction.exec=execute
-  $.keyboard.defaultOptions.combos={}
-  $.keyboard.comboRegex=/(`)(.)/mig
-  $(I.code).addTyping().keyboard({
-    layout:'custom',useCombos:false,autoAccept:true,usePreview:false,customLayout:layout,useCombos:true,combos:combos,
-    display:{bksp:'Bksp',shift:'⇧',alt:'APL',enter:'Enter',exec:'⍎'}
-  })
-  I.code.focus()
-  I.code.onkeydown=x=>{if(x.which===13&&x.ctrlKey){I.go.click();return!1}}
-
-  var tipsyOpts={gravity:'s',delayIn:1000,opacity:1,title:_=>hSymbolDefs[$(this).text()]||''}
-  $('.ui-keyboard').on('mouseover','.ui-keyboard-button',event=>{
-    var $b=$(event.target).closest('.ui-keyboard-button')
-    if(!$b.data('tipsyInitialised'))$b.data('tipsyInitialised',1).tipsy(tipsyOpts).tipsy('show')
+  return!1
+}
+hp.run&&I.go.click()
+I.code.focus()
+I.code.onkeydown=x=>{
+  const m=x.ctrlKey+2*x.shiftKey+4*x.altKey+8*x.metaKey
+  if(x.which===13&&m===1){I.go.click();return!1}
+  if(x.which===9&&!m){
+    const e=I.code,i=e.selectionStart,v=e.value,s=v.slice(i-2,i),c=tc[s]
+    if(c){e.value=v.slice(0,i-2)+c+v.slice(i);e.selectionStart=e.selectionEnd=i-1}
     return!1
-  })
-
-  // Tests
-  const get=(x,f)=>{
-    var xhr=new XMLHttpRequest;xhr.open('get',x)
-    xhr.onreadystatechange=x=>{xhr.readyState===4&&f(xhr.responseText)}
-    xhr.send()
-  }
-  const runDocTests=_=>{
-    get('../apl.js',x=>{
-      const aplTests=collectTests(x)
-      I.result.classList.remove('error');I.result.classList.textContent=''
-      var ne=0,nf=0,t0=+new Date // ne:number of executed, nf:number of failed
-      for(var i=0;i<aplTests.length;i++){
-        var x=aplTests[i],code=x[0],mode=x[1],expectation=x[2]
-        ne++
-        var o=runDocTest([code,mode,expectation],apl,apl.aprx)
-        if(o){
-          nf++
-          var s='Test failed: '+JSON.stringify(code)+'\n'+
-                '             '+JSON.stringify(expectation)+'\n'
-          if(o.reason)s+=o.reason+'\n'
-          if(o.error)s+=o.error.stack+'\n'
-          I.result.textContent+=s
-        }
-      }
-      I.result.textContent+=(nf?nf+' out of '+ne+' tests failed':'All '+ne+' tests passed')+' in '+(new Date-t0)+' ms.\n'
-    })
   }
 }
+const hc={'<':'&lt;','&':'&amp;',"'":'&apos;','"':'&quot;'},he=x=>x.replace(/[<&'"]/g,c=>hc[c])
+const tcs='<-←xx×:-÷*o⍟[-⌹oo○ff⌈FF⌈ll⌊LL⌊tt⊥TT⊤-|⊣|-⊢~~≈=/≠<=≤>=≥==≡=-≢vv∨^^∧^~⍲v~⍱^|↑v|↓<<⊂>>⊃[|⌷A|⍋V|⍒ii⍳ee∊e-⍷'+
+'uu∪nn∩/-⌿\\-⍀,-⍪rr⍴pp⍴o|⌽o-⊖o\\⍉..¨~:⍨*:⍣o.∘[\'⍞[]⎕[:⍠[=⌸ot⍎oT⍕<>⋄on⍝aa⍺ww⍵a-⍶w-⍹VV∇--¯88∞0~⍬V~⍫//↗[/⍁'
+const lbs=['←assign','+add;conjugate','-sub;negate','×multiply;signum','÷divide;reciprocal','*power;exp','⍟log;ln',
+'⌹matrix divide;matrix inverse','○circular;pi','!binomial;factorial','?deal;roll','|residue;magnitude',
+'⌈max;ceiling','⌊min;floor','⊥decode','⊤encode','⊣left','⊢right','=equals','≈approx','≠not equals',
+'≤lesser or equal to','>less than','<greater than','≥greater or equal to','≡match','≢not match','∨or',
+'∧and','⍲nand','⍱nor','↑take;mix','↓drop;split','⊂enclose','⊃pick;first','⌷index','⍋grade up',
+'⍒grade down','⍳index of;indices','∊member of','⍷find','∪union;unique','∩intersection','~without;not',
+'/reduce;replicate','\\scan','⌿1st axis reduce;1st axis replicate','⍀1st axis scan',',flatten;catenate',
+'⍪table;1st axis catenate','⍴reshape;shape of','⌽rotate;reverse','⊖1st axis rotate;1st axis reverse',
+'⍉reorder axes;transpose','¨each','⍨commute;selfie','⍣power operator','.inner product',
+'∘compose;outer product','⍞stdin;stdout','⎕"eval\'ed stdin;stdout"','⍠combine monadic-dyadic fns',
+'⌸key operator','⍎execute','⍕format','⋄statement separator','⍝comment','⍺left argument','⍵right argument',
+'⍶left operand','⍹right operand','∇recursion','¯negative','∞infinity','⍬empty numeric vector',
+'⍫"return" reified as a function','↗throw','⍁identity element operator']
+const tc={};for(let i=0;i<tcs.length;i+=3)tc[tcs[i]+tcs[i+1]]=tc[tcs[i+1]+tcs[i]]=tcs[i+2] // tab completions
+let lbh='';for(let i=0;i<lbs.length;i++){
+  const ks=[];for(let j=0;j<tcs.length;j+=3)if(tcs[j+2]===lbs[i][0])ks.push('\n'+tcs[j]+' '+tcs[j+1]+' <tab>')
+  lbh+='<b title="'+he(lbs[i].slice(1))+(ks.length?'\n'+ks.join(''):'')+'">'+lbs[i][0]+'</b>'
+}
+I.lb.innerHTML=lbh
+I.lb.onmousedown=x=>{
+  if(x.target.nodeName!=='B')return
+  const e=I.code,i=e.selectionStart,j=e.selectionEnd,v=e.value,s=x.target.textContent
+  if(i!=null&&j!=null){e.value=v.slice(0,i)+s+v.slice(j);e.selectionStart=e.selectionEnd=i+s.length}
+  return!1
+}
+const get=(x,f)=>{const r=new XMLHttpRequest;r.open('get',x)
+                  r.onreadystatechange=x=>{r.readyState===4&&f(r.responseText)};r.send()}
+const runDocTests=_=>{
+  get('../apl.js',x=>{
+    const aplTests=collectTests(x)
+    I.result.classList.remove('err');I.result.classList.textContent=''
+    let ne=0,nf=0,t0=+new Date // ne:number of executed, nf:number of failed
+    for(let i=0;i<aplTests.length;i++){
+      ne++;let x=aplTests[i],code=x[0],mode=x[1],expectation=x[2],o=runDocTest([code,mode,expectation],apl,apl.aprx)
+      if(o){
+        nf++
+        let s='Test failed: '+JSON.stringify(code)+'\n'+
+              '             '+JSON.stringify(expectation)+'\n'
+        if(o.reason)s+=o.reason+'\n'
+        if(o.error)s+=o.error.stack+'\n'
+        I.result.textContent+=s
+      }
+    }
+    I.result.textContent+=(nf?nf+' out of '+ne+' tests failed':'All '+ne+' tests passed')+' in '+(new Date-t0)+' ms.\n'
+  })
+}
+})();
