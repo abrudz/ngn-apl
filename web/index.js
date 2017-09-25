ed35c7bbd94dd38ff4458d86b5cbad2156f66c4f
- <@>
2017-09-25 19:31:22 +0300

    backquote-prefixed character entry

--- web/index.js
+++ web/index.js
@@ -14,12 +14,22 @@ I.go.onclick=_=>{
 }
 hp.run&&I.go.click()
 I.code.focus()
-I.code.onkeydown=x=>{switch(x.ctrlKey+2*x.shiftKey+4*x.altKey+8*x.metaKey+100*x.which){
-  case 1301:{I.go.click();return!1}
-  case  900:{const e=I.code,i=e.selectionStart,v=e.value,s=v.slice(i-2,i),c=tc[s]
-             if(c){e.value=v.slice(0,i-2)+c+v.slice(i);e.selectionStart=e.selectionEnd=i-1}
-             return!1}
-}}
+let bqm=0
+I.code.onkeydown=x=>{
+  const k=x.ctrlKey+2*x.shiftKey+4*x.altKey+8*x.metaKey+100*x.which
+  if(bqm){
+    const e=I.code,i=e.selectionStart,v=e.value,c=bqc[x.key]
+    if(x.which>31){bqm=0;e.classList.remove('bq')}
+    if(c){e.value=v.slice(0,i)+c+v.slice(i);e.selectionStart=e.selectionEnd=i+1;return!1}
+  }
+  switch(k){
+    case 19200:bqm=1;I.code.classList.add('bq');return!1 // `
+    case  1301:{I.go.click();return!1} // ctrl-enter
+    case   900:{const e=I.code,i=e.selectionStart,v=e.value,c=tc[v.slice(i-2,i)] // tab
+                if(c){e.value=v.slice(0,i-2)+c+v.slice(i);e.selectionStart=e.selectionEnd=i-1}
+                return!1}
+  }
+}
 const hc={'<':'&lt;','&':'&amp;',"'":'&apos;','"':'&quot;'},he=x=>x.replace(/[<&'"]/g,c=>hc[c])
 ,tcs='<-←xx×:-÷*o⍟[-⌹oo○ff⌈FF⌈ll⌊LL⌊tt⊥TT⊤-|⊣|-⊢~~≈=/≠<=≤>=≥==≡=-≢vv∨^^∧^~⍲v~⍱^|↑v|↓<<⊂>>⊃[|⌷A|⍋V|⍒ii⍳ee∊e-⍷'+
 'uu∪nn∩/-⌿\\-⍀,-⍪rr⍴pp⍴o|⌽o-⊖o\\⍉..¨~:⍨*:⍣o.∘[\'⍞[]⎕[:⍠[=⌸ot⍎oT⍕<>⋄on⍝aa⍺ww⍵a-⍶w-⍹VV∇--¯88∞0~⍬V~⍫//↗[/⍁'
@@ -36,11 +46,16 @@ const hc={'<':'&lt;','&':'&amp;',"'":'&apos;','"':'&quot;'},he=x=>x.replace(/[<&
 '⌸key operator','⍎execute','⍕format','⋄statement separator','⍝comment','⍺left argument','⍵right argument',
 '⍶left operand','⍹right operand','∇recursion','¯negative','∞infinity','⍬empty numeric vector',
 '⍫"return" reified as a function','↗throw','⍁identity element operator']
-,tc={} // tab completions
+,bqk=' =1234567890-qwertyuiop\\asdfghjkl;\'zxcvbnm,./`[]+!@#$%^&*()_QWERTYUIOP|ASDFGHJKL:"ZXCVBNM<>?~{}'
+,bqv='`÷¨¯<≤=≥>≠∨∧×?⍵∊⍴~↑↓⍳○*⊢ ⍺⌈⌊_∇∆∘k⎕⍎⍕ ⊂⊃∩∪⊥⊤|⍝⍀⌿⋄←→⌹⌶⍫⍒⍋⌽⍉⊖⍟⍱⍲!⍰⍵⍷⍷⍨↑↓⍸⍥⍣⊣⍺⌈⌊_⍢HJ⌸⌷≡≢ZXCV⍭⍡∥⍪⍙⍠¤⍞⍬'.replace(/ /g,'')
+,tc={},bqc={}
+for(let i=0;i<bqk.length;i++)bqc[bqk[i]]=bqv[i]
 for(let i=0;i<tcs.length;i+=3)tc[tcs[i]+tcs[i+1]]=tcs[i+2]
 for(let i=0;i<tcs.length;i+=3){let k=tcs[i+1]+tcs[i];tc[k]=tc[k]||tcs[i+2]}
 let lbh='';for(let i=0;i<lbs.length;i++){
-  const ks=[];for(let j=0;j<tcs.length;j+=3)if(tcs[j+2]===lbs[i][0])ks.push('\n'+tcs[j]+' '+tcs[j+1]+' <tab>')
+  const ks=[]
+  for(let j=0;j<tcs.length;j+=3)if(lbs[i][0]===tcs[j+2])ks.push('\n'+tcs[j]+' '+tcs[j+1]+' <tab>')
+  for(let j=0;j<bqk.length;j++)if(lbs[i][0]===bqv[j])ks.push('\n` '+bqk[j])
   lbh+='<b title="'+he(lbs[i].slice(1))+(ks.length?'\n'+ks.join(''):'')+'">'+lbs[i][0]+'</b>'
 }
 I.lb.innerHTML=lbh
@@ -54,7 +69,7 @@ const get=(x,f)=>{const r=new XMLHttpRequest;r.open('get',x)
                   r.onreadystatechange=x=>{r.readyState===4&&f(r.responseText)};r.send()}
 ,runDocTests=_=>{get('../t.apl',x=>{
   const t=collectTests(x)
-  I.rslt.classList.remove('err');I.rslt.classList.textContent=''
+  I.rslt.classList.remove('err');I.rslt.textContent=''
   let ne=0,nf=0,t0=+new Date // ne:number of executed, nf:number of failed
   for(let i=0;i<t.length;i++){
     ne++;let x=t[i],o=runDocTest(x,apl,apl.aprx)
