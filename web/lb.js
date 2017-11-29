7d290baf782f7901f3d6d0b8d3cc6269a84a03b6
- <@>
2017-11-29 18:33:09 +0200

    lb: revised tooltips

--- web/lb.js
+++ web/lb.js
@@ -2,19 +2,16 @@
 let hc={'<':'&lt;','&':'&amp;',"'":'&apos;','"':'&quot;'},he=x=>x.replace(/[<&'"]/g,c=>hc[c]) //html chars and escape fn
 ,tcs='<-←xx×:-÷*o⍟[-⌹oo○ff⌈FF⌈ll⌊LL⌊tt⊥TT⊤-|⊣|-⊢~~≈=/≠<=≤>=≥==≡=-≢vv∨^^∧^~⍲v~⍱^|↑v|↓<<⊂>>⊃[|⌷A|⍋V|⍒ii⍳ee∊e-⍷'+
 'uu∪nn∩/-⌿\\-⍀,-⍪rr⍴pp⍴o|⌽o-⊖o\\⍉..¨~:⍨*:⍣o.∘[\'⍞[]⎕[:⍠[=⌸ot⍎oT⍕<>⋄on⍝aa⍺ww⍵a-⍶w-⍹VV∇--¯88∞0~⍬V~⍫//↗[/⍁'
-,lbs=['←assign','+conjugate;add','-negate;subtract','×signum;multiply','÷reciprocal;divide','*exp;power','⍟ln;log',
-'⌹matrix inverse;matrix divide','○pi;circular','!factorial;binomial','?roll;deal','|magnitude;residue',
-'⌈ceiling;max','⌊floor;min','⊥decode','⊤encode','⊣left','⊢right','=equals','≈approx','≠not equals',
-'≤lesser or equal to','<less than','>greater than','≥greater or equal to','≡depth;match','≢tally;not match','∨or',
-'∧and','⍲nand','⍱nor','↑mix;take','↓split;drop','⊂enclose','⊃first;pick','⌷index','⍋grade up',
-'⍒grade down','⍳indices;index of','∊flatten;member of','⍷find','∪unique;union','∩intersection','~not;without',
-'/reduce;replicate','\\scan','⌿1st axis reduce;1st axis replicate','⍀1st axis scan',',enlist;catenate',
-'⍪table;1st axis catenate','⍴shape of;reshape','⌽reverse;rotate','⊖1st axis reverse;1st axis rotate',
-'⍉transpose;reorder axes','¨each','⍨selfie;commute','⍣power operator','.inner product',
-'∘compose','⍞stdin/stdout','⎕eval\'ed stdin/stdout','⍠combine monadic-dyadic fns',
-'⌸key operator','⍎execute','⍕format','⋄statement separator','⍝comment','⍺left argument','⍵right argument',
-'⍶left operand','⍹right operand','∇recursion','¯negative','∞infinity','⍬empty numeric vector',
-'⍫"return" reified as a function','↗throw','⍁identity element operator']
+,lbs=`←assign_+conjugate;add_-negate;subtract_×signum;multiply_÷reciprocal;divide_*exp;power_⍟ln;log
+⌹matrix inverse;matrix divide_○pi;circular_!factorial;binomial_?roll;deal_|magnitude;residue_⌈ceiling;max_⌊floor;min
+⊥decode_⊤encode_⊣left_⊢right_=equals_≈approx_≠not equals_≤lesser or equal to_<less than_>greater than
+≥greater or equal to_≡depth;match_≢tally;not match_∨or_∧and_⍲nand_⍱nor_↑mix;take_↓split;drop_⊂enclose_⊃first;pick
+⌷index_⍋grade up_⍒grade down_⍳indices;index of_∊flatten;member of_⍷find_∪unique;union_∩intersection_~not;without
+/reduce;replicate_\\scan_⌿1st axis reduce;1st axis replicate_⍀1st axis scan_,enlist;catenate_⍪table;1st axis catenate
+⍴shape of;reshape_⌽reverse;rotate_⊖1st axis reverse;1st axis rotate_⍉transpose;reorder axes_¨each_⍨selfie;commute
+⍣power operator_.inner product_∘compose_⍞stdin/stdout_⎕eval\'ed stdin/stdout_⍠combine monadic-dyadic fns_⌸key operator
+⍎execute_⍕format_⋄statement separator_⍝comment_⍺left argument_⍵right argument_⍶left operand_⍹right operand_∇recursion
+¯negative_∞infinity_⍬empty numeric vector_⍫"return" reified as a function_↗throw_⍁identity element operator`.split(/[\n_]/)
 ,bqk=' =1234567890-qwertyuiop\\asdfghjkl;\'zxcvbnm,./`[]+!@#$%^&*()_QWERTYUIOP|ASDFGHJKL:"ZXCVBNM<>?~{}'
 ,bqv='`÷¨¯<≤=≥>≠∨∧×?⍵∊⍴~↑↓⍳○*⊢ ⍺⌈⌊_∇∆∘k⎕⍎⍕ ⊂⊃∩∪⊥⊤|⍝⍀⌿⋄←→⌹⌶⍫⍒⍋⌽⍉⊖⍟⍱⍲!⍰⍵⍷⍷⍨↑↓⍸⍥⍣⊣⍺⌈⌊_⍢HJ⌸⌷≡≢ZXCV⍭⍡∥⍪⍙⍠¤⍞⍬'.replace(/ /g,'')
 ,tc={},bqc={} //tab completions and ` completions
@@ -25,10 +22,10 @@ let lbh='';for(let i=0;i<lbs.length;i++){
   let ks=[]
   for(let j=0;j<tcs.length;j+=3)if(lbs[i][0]===tcs[j+2])ks.push('\n'+tcs[j]+' '+tcs[j+1]+' <tab>')
   for(let j=0;j<bqk.length;j++)if(lbs[i][0]===bqv[j])ks.push('\n` '+bqk[j])
-  lbh+='<b title="'+he(lbs[i].slice(1))+(ks.length?'\n'+ks.join(''):'')+'">'+lbs[i][0]+'</b>'
+  lbh+='<b title="'+he(lbs[i].slice(1).replace(';','\n'))+(ks.length?'\n'+ks.join(''):'')+'">'+lbs[i][0]+'</b>'
 }
 let d=document,el=d.createElement('div');el.innerHTML=
-`<div class=ngn_lb><span class=ngn_x title=Close>❎</span>${lbh}</div>
+`<div class=ngn_lb><span class=ngn_x title="Hide language bar">❎</span>${lbh}</div>
  <style>@font-face{font-family:"Apl385 Unicode";src:url(Apl385.woff)format('woff');}</style>
  <style>
   .ngn_lb{position:fixed;top:0;left:0;right:0;background-color:#eee;color:#000;cursor:default;z-index:2147483647;
